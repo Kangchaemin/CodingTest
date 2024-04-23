6d07dcdca3dff1cc4ca95dc3bfc116955d8cd1ee
Kangchaemin <43837994+Kangchaemin@users.noreply.github.com>
2024-04-23 16:11:43 +0900

    Create 정렬_문자열 내 마음대로 정렬하기.java

---
+++ 정렬/정렬_문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        
+        Arrays.sort(strings, (s1, s2) -> {
+            if(s1.charAt(n) == s2.charAt(n)){
+                return s1.compareTo(s2);
+            }
+            return s1.charAt(n) - s2.charAt(n);
+        });
+        
+        // 같은 내용이다.
+        // Arrays.sort(strings, new Comparator<String>(){
+        //     @Override 
+        //     public int compare(String s1, String s2){
+        //         if(s1.charAt(n) == s2.charAt(n)){
+        //             return s1.compareTo(s2);
+        //         }
+        //         return s1.charAt(n) - s2.charAt(n);
+        //     }
+        // });
+        
+        return strings;
+    }
+}
